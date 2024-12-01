812180871a4bfaa2b33c357e4177f29b71955969
ptho1504 <phuoctho150420@gmail.com>
2024-12-02 01:14:59 +0700

    add Login Screen

--- services/base.ts
+++ services/base.ts
@@ -1,18 +1,19 @@
-import { Config } from '@/config';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Config } from "@/config";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   BaseQueryApi,
+  BaseQueryResult,
   createApi,
   FetchArgs,
   fetchBaseQuery,
-} from '@reduxjs/toolkit/query/react';
+} from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: Config.API_URL,
   prepareHeaders: async (headers, api) => {
-    const token = await AsyncStorage.getItem('user');
+    const token = await AsyncStorage.getItem("user");
     if (token) {
-      headers.set('authorization', `Bearer ${token}`);
+      headers.set("authorization", `Bearer ${token}`);
     }
     return headers;
   },
@@ -21,12 +22,17 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithInterceptor = async (
   args: string | FetchArgs,
   api: BaseQueryApi,
-  extraOptions: {},
-) => {
+  extraOptions: {}
+): Promise<BaseQueryResult<any>> => {
   const result = await baseQuery(args, api, extraOptions);
-  if (result.error && result.error.status === 401) {
-    // here you can deal with 401 error
-  }
+  // if (result.error && result.error.status === 401) {
+  // here you can deal with 401 error
+  // }
+
+  // if (result.error && result.error.data) {
+  //   return result.error.data as any;
+  // }
+
   return result;
 };
 
